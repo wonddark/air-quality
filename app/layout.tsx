401acb534c5727ede5c1722d7815e565da0ff278
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-01-06 17:00:36 -0500

    Updated the main layout to allow the use of more space

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,20 +11,26 @@ export const metadata: Metadata = {
   description: "Check Air Quality Index for cities and countries",
 };
 
-type Props = { children: ReactNode };
+type Props = { children: ReactNode; countries: ReactNode };
 
-export default function RootLayout({ children }: Readonly<Props>) {
+export default function RootLayout({ children, countries }: Readonly<Props>) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <header className="bg-sky-600">
-          <div className="py-5 px-3 max-w-4xl mx-auto">
-            <a href="/" className="text-xl text-slate-200">
+          <div className="py-5 px-3 max-w-6xl mx-auto">
+            <Link
+              href="/"
+              className="text-xl text-slate-200 hover:brightness-125"
+            >
               Check AQI For
-            </a>
+            </Link>
           </div>
         </header>
-        <div className="max-w-4xl mx-auto mt-5 px-3">{children}</div>
+        <div className="max-w-6xl mx-auto mt-5 px-3 grid grid-cols-[30%_70%]">
+          <div>{countries}</div>
+          <div>{children}</div>
+        </div>
       </body>
     </html>
   );
