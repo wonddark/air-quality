a7736d50f968c94f6f22c1e23d4f148c582a2a52
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-01-06 18:07:34 -0500

    Fixed format

--- app/@countries/page.tsx
+++ app/@countries/page.tsx
@@ -1,8 +1,8 @@
 import CountryItem from "@app/app/@countries/CountryItem";
 
 export default async function CountriesList() {
-  const { data } = await fetch(`${process.env.BASE_PATH}/countries`).then((res) =>
-    res.json()
+  const { data } = await fetch(`${process.env.BASE_PATH}/countries`).then(
+    (res) => res.json()
   );
   return (
     <ul className="h-[calc(100vh_-_68px_-_3rem)] overflow-auto mr-3">
