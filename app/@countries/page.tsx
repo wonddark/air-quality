95ff5c75522ce6d2088b79fcf7025f696465e3f4
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-01-06 17:06:10 -0500

    Parallel route to be loaded in the sidebar of main layout

---
+++ app/@countries/page.tsx
@@ -0,0 +1,14 @@
+import CountryItem from "@app/app/@countries/CountryItem";
+
+export default async function CountriesList() {
+  const { data } = await fetch("http://localhost:3000/countries").then((res) =>
+    res.json()
+  );
+  return (
+    <ul className="h-[calc(100vh_-_68px_-_3rem)] overflow-auto mr-3">
+      {data.map((item: { country: string; cities: string[] }) => (
+        <CountryItem item={item} key={item.country} />
+      ))}
+    </ul>
+  );
+}
