ddcf000eda60bb019157f4ca094bc72377234167
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-01-06 18:20:52 -0500

    Fixed format

--- app/city/[city]/page.tsx
+++ app/city/[city]/page.tsx
@@ -10,12 +10,9 @@ export function generateMetadata({ params: { city } }: Readonly<Props>) {
 }
 
 async function getAQIForCity(city: string) {
-  const res = await fetch(
-    `${process.env.API_AQI_BASE_URL}?city=${city}`,
-    {
-      headers: { "x-api-key": process.env.API_KEY },
-    }
-  );
+  const res = await fetch(`${process.env.API_AQI_BASE_URL}?city=${city}`, {
+    headers: { "x-api-key": process.env.API_KEY },
+  });
   return await res.json();
 }
 
