f03111d60276e25faff64919363ace1dc10b9038
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-01-07 18:35:51 -0500

    Updated the layout for better display in mobile views

--- app/@countries/page.tsx
+++ app/@countries/page.tsx
@@ -1,14 +1,18 @@
 import CountryItem from "@app/app/@countries/CountryItem";
+import CitySelect from "@app/app/@countries/CitySelect";
 
 export default async function CountriesList() {
   const res = await fetch(`${process.env.API_COUNTRIES_URL}`);
   const { data } = await res.json();
 
   return (
-    <ul className="h-[calc(100vh_-_68px_-_3rem)] overflow-auto mr-3">
-      {data.map((item: { country: string; cities: string[] }) => (
-        <CountryItem item={item} key={item.country} />
-      ))}
-    </ul>
+    <>
+      <CitySelect data={data} />
+      <ul className="hidden lg:block h-[calc(100vh_-_68px_-_3rem)] overflow-auto mr-3">
+        {data.map((item: { country: string; cities: string[] }) => (
+          <CountryItem item={item} key={item.country} />
+        ))}
+      </ul>
+    </>
   );
 }
